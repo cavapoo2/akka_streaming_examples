4f9b2b86d6b4701f9ffd2ee72b24caf3d4598f4e
andyr <forpetessake@protonmail.com>
2017-09-15 19:59:42 +0100

    Add files via upload

---
+++ project/Dependencies.scala
@@ -0,0 +1,8 @@
+import sbt._
+import Keys._
+object Dependencies {
+  val scalatest = "org.scalatest" %% "scalatest" % "3.0.1"
+  val akka = "com.typesafe.akka" % "akka-actor_2.12" % "2.5.4"
+  val akka_stream = "com.typesafe.akka" % "akka-stream_2.12" % "2.5.4"
+
+}

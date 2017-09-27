40b924e3c056424b6045158e0f91bde0a0ced94e
andyr <forpetessake@protonmail.com>
2017-09-27 14:34:52 +0100

    Add files via upload

--- src/main/scala/anotherProcFiles.scala
+++ src/main/scala/anotherProcFiles.scala
@@ -1,7 +1,10 @@
 package com.andyr
+import java.nio.file.{Path, Paths}
+
 import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
-import akka.stream.scaladsl.{Sink, Source}
+import akka.stream.scaladsl.{FileIO, Flow, Framing, Sink, Source}
+import akka.util.ByteString
 
 import scala.io.Source._
 object AnotherProcFiles extends App {
@@ -15,6 +18,15 @@ object AnotherProcFiles extends App {
       .map(_.mkString("\n"))
       .runWith(Sink.foreach(println(_)))
   }
+
+  def procFiles2(f:List[String]) : Unit = {
+    val lines = Flow[ByteString].via(Framing.delimiter(ByteString("\n"),10000,true))
+        .map(_.utf8String)
+    val readFile = Flow[Path].flatMapConcat{path => FileIO.fromPath(path).via(lines)}
+    Source(f).map(Paths.get(_)).via(readFile).runWith(Sink.foreach(println(_)))
+  }
   procFiles(List("src/main/resources/another1.txt","src/main/resources/another2.txt"))
+  procFiles2(List("src/main/resources/another1.txt","src/main/resources/another2.txt"))
+
 }
 
